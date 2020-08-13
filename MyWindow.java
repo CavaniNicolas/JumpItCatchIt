574cc0ca098e3c67b0beead6ec722e9fcd6a9d87
Cavani Nicolas <cavani.nicolas6@gmail.com>
2020-08-14 00:48:03 +0200

    create window

--- MyWindow.java
+++ MyWindow.java
@@ -1,13 +1,25 @@
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class MyWindow extends JFrame {
 
 	public MyWindow() {
 		this.setTitle("Jump It Catch It");
-		this.setSize(800, 800);
-		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Fenetre hors plein ecran : width = 80% et height = 90% (les proportions sont a peu pres conservees)
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)(screenSize.getWidth()*0.8);
+		int height = (int)(screenSize.getHeight()*0.9);
+		this.setSize(width, height);
+		this.setLocation( (int)(screenSize.getWidth() * 0.1), (int)(screenSize.getHeight() * 0.05) );
+
+		// Plein ecran
+		// this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// this.setUndecorated(true);
 
 		this.setVisible(true);
 	}
