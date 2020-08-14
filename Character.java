fd845801fb3a7de8232f4d6e6a332c59773ce177
Cavani Nicolas <cavani.nicolas6@gmail.com>
2020-08-14 03:21:27 +0200

    start creating fields for Character

--- Character.java
+++ Character.java
@@ -1,7 +1,27 @@
+import java.awt.Color;
+import java.awt.Image;
 
 public class Character {
-	
+
 	KeyBindings keyBindings;
+
+	/**Nombre de vies (en moities de coeur) */
+	private int lives = 6;
+
+	// Coordonnees
+	private int x;
+	private int y;
+
+	// Booleens d'autorisation d'actions
+	private boolean canJump;
+	private boolean canGrab;
+	private boolean canShield;
+	private boolean canShoot;
+	private boolean canPush;
+
+
+	private Image imageCharacter;
+	private Color colorCharacter;
 
 
 	public Character() {
