5d1602c67d0a7b95bec1a9ceaf91cdefc9948dcd
NowSayMyName <yoann.rouxel@insa-lyon.fr>
2020-08-23 00:22:39 +0200

    added getadresses

---
+++ Network/GetAddresses.java
@@ -0,0 +1,35 @@
+package Network;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class GetAddresses {
+	public static void main(String[] args) {
+		try {
+		   Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces();
+		   
+		   while(list.hasMoreElements()){
+			  
+			  NetworkInterface ni = list.nextElement();
+			  Enumeration<InetAddress> listAddress = ni.getInetAddresses();
+			  
+			  while(listAddress.hasMoreElements()){
+				 InetAddress address = listAddress.nextElement();
+				 showInformations(address);
+			  }
+			  
+		   }
+		} catch (SocketException e) {
+		   e.printStackTrace();
+		}
+	 }
+	 
+	 public static void showInformations(InetAddress address){
+		System.out.println("-----------------------------------------------");
+		System.out.println("Nom  : " + address.getHostName());
+		System.out.println("Adresse : " + address.getHostAddress());
+		System.out.println("Nom canonique : " + address.getCanonicalHostName());
+	 }
+}
